7640cf316c0192f5d15ffa48a52ee9e6aee421a8
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-05-05 15:14:09 +0100

    create ui of the extention

--- script.ts
+++ script.ts
@@ -1,1 +1,19 @@
 const world: string = "hello world"
+
+
+const switchElement = document.getElementById("switch")
+
+if(switchElement) {
+    const toggleSwitch = () => {
+        const switchState = switchElement.getAttribute('aria-checked') === "true"
+        switchElement.setAttribute('aria-checked', String(!switchState))
+    }
+    switchElement.addEventListener('click', () => {
+        toggleSwitch()
+    })
+    switchElement.addEventListener('keydown', (e) => {
+          if(e.key === "Enter" || e.key === " ") {
+            toggleSwitch()
+          }
+    })
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js,ts}"],
   theme: {
-    extend: {},
+    backgroundColor: {
+"light-blue": "#F1F4F9",
+"dark-grey": "#41474D",
+"blue-sky": "#D3E5F5",
+    },
+    colors: {
+      "dark-grey": "#41474D",
+      "blackish": "#0C1D29",
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
