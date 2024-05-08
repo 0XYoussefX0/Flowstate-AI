b9d30c1efc28dfd117ba1017eb0d28a65aa1f709
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-05-08 12:22:49 +0100

    restrict user access to youtube shorts

--- background.ts
+++ background.ts
@@ -1,13 +1,18 @@
 // this code was part of the code that i've copied from a tutorial on freecodecamp
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  console.log("oga bonga");
+chrome.tabs.onUpdated.addListener(async (tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
 
     chrome.tabs.sendMessage(tabId, {
-      type: "NEW",
+      type: "new video",
       videoId: urlParameters.get("v"),
     });
+  } else if (tab.url && tab.url.includes("youtube.com/shorts")) {
+    const state = await chrome.storage.local.get("switchState");
+    console.log(state);
+    if (state.switchState === true) {
+      chrome.tabs.goBack(tabId);
+    }
   }
 });
