284696a0e5515f90020a6565c86de8d433aadb14
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-05-08 12:22:19 +0100

    show a modal when the user is not allowed to watch the video

--- script.ts
+++ script.ts
@@ -107,6 +107,35 @@
     removeShorts();
   }
 
+  const showModal = (encouragement: string): void => {
+    // the next 5 lines were copied from ChatGPT
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "./modalStyles.css";
+    document.head.appendChild(link);
+
+    // add an element that will act as an overlay that will make the background look blurry
+    const modalWrapper = document.createElement("div");
+    modalWrapper.classList.add("modalWrapper");
+    const warningIcon = document.createElement("img");
+    warningIcon.src = "./assets/warningIcon.svg";
+    warningIcon.alt = "";
+    const modalHeader = document.createElement("h1");
+    modalHeader.textContent = "You are not allowed to watch this.";
+    const modalParagraph = document.createElement("p");
+    modalParagraph.textContent =
+      "Ahoy there, front-end developer! It seems like you've navigated to the wrong port of knowledge. While weaving baskets under the sea may sound like a creative endeavor, let's keep our heads above water and our fingers on the keyboard for now. Time to surface from the depths of aquatic craft and set sail for the shores of code! Remember, the only weaving we're doing here is weaving lines of code, not weaving baskets! 💻🌊";
+    const modalLink = document.createElement("a");
+    modalLink.textContent = "Go back to the home page";
+    modalLink.href = "https://www.youtube.com";
+    const runningIcon = document.createElement("img");
+    runningIcon.src = "./assets/runningIcon.svg";
+    runningIcon.alt = "";
+
+    modalLink.appendChild(runningIcon);
+  };
+
   chrome.runtime.onMessage.addListener(
     async (request, sender, sendResponse) => {
       if (request.action === "shorts off") {
@@ -118,6 +147,22 @@
         );
         if (response.ok) {
           const data = await response.json();
+          const inputData = await chrome.storage.local.get("textInput");
+          // perform client side validation to ensure that the inputs follow the format that the ai accepts
+          const requestBody = { Goal: inputData, videoTranscript: data };
+          const geminiResponse = await fetch("http://127.0.0.1:5000/gemini", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestBody),
+          });
+          if (response.ok) {
+            const geminiOutput = await geminiResponse.json();
+            if (geminiOutput.verdict === "Not Allowed") {
+              showModal(geminiOutput.encouragement);
+            }
+          }
         }
       }
     }
