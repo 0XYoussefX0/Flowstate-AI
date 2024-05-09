edc2638897aee9bd09c7d393663d8cc0aa1181da
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-05-09 11:51:17 +0100

    fix modal styling

--- script.ts
+++ script.ts
@@ -16,35 +16,29 @@
   };
 
   const removeShortsLink = () => {
-    return new Promise((resolve, reject) => {
-      const shortsLink = document.querySelector(
-        "#items > ytd-guide-entry-renderer:nth-child(2)"
-      );
-      if (shortsLink) {
-        shortsLink.remove();
-        resolve(true);
-      } else {
-        setTimeout(() => {
-          removeShortsLink();
-        }, 100);
-      }
-    });
+    const shortsLink = document.querySelector(
+      "#items > ytd-guide-entry-renderer:nth-child(2)"
+    );
+    if (shortsLink) {
+      shortsLink.remove();
+    } else {
+      setTimeout(() => {
+        removeShortsLink();
+      }, 100);
+    }
   };
 
   const removeShortsButton = () => {
-    return new Promise((resolve, reject) => {
-      const shortsButton = document.querySelector(
-        "#chips > yt-chip-cloud-chip-renderer:nth-child(2)"
-      );
-      if (shortsButton) {
-        shortsButton.remove();
-        resolve(true);
-      } else {
-        setTimeout(() => {
-          removeShortsButton();
-        }, 100);
-      }
-    });
+    const shortsButton = document.querySelector(
+      "#chips > yt-chip-cloud-chip-renderer:nth-child(2)"
+    );
+    if (shortsButton) {
+      shortsButton.remove();
+    } else {
+      setTimeout(() => {
+        removeShortsButton();
+      }, 100);
+    }
   };
 
   const getTargetNode = () => {
@@ -84,12 +78,12 @@
             shortReels.remove();
           }
           //this code should be improved to be faster
-          const videoList = document.querySelectorAll("ytd-video-renderer");
-          videoList.forEach((video) => {
-            if (isShortVideo(video)) {
-              video.remove();
-            }
-          });
+          // const videoList = document.querySelectorAll("ytd-video-renderer");
+          // videoList.forEach((video) => {
+          //   if (isShortVideo(video)) {
+          //     video.remove();
+          //   }
+          // });
         }
       }
     };
@@ -108,62 +102,89 @@
   }
 
   const showModal = (encouragement: string): void => {
+    console.log(encouragement);
     // the next 5 lines were copied from ChatGPT
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href = "./modalStyles.css";
+    link.href = chrome.runtime.getURL("./modalStyles.css");
     document.head.appendChild(link);
+
+    const body = document.querySelector("body");
 
     // add an element that will act as an overlay that will make the background look blurry
     const modalWrapper = document.createElement("div");
     modalWrapper.classList.add("modalWrapper");
+
+    const backdrop = document.createElement("div");
+    backdrop.classList.add("backdrop");
+
     const warningIcon = document.createElement("img");
-    warningIcon.src = "./assets/warningIcon.svg";
+    warningIcon.src = chrome.runtime.getURL("./assets/warningIcon.svg");
     warningIcon.alt = "";
+
     const modalHeader = document.createElement("h1");
     modalHeader.textContent = "You are not allowed to watch this.";
+
     const modalParagraph = document.createElement("p");
-    modalParagraph.textContent =
-      "Ahoy there, front-end developer! It seems like you've navigated to the wrong port of knowledge. While weaving baskets under the sea may sound like a creative endeavor, let's keep our heads above water and our fingers on the keyboard for now. Time to surface from the depths of aquatic craft and set sail for the shores of code! Remember, the only weaving we're doing here is weaving lines of code, not weaving baskets! 💻🌊";
+    modalParagraph.textContent = encouragement;
+
     const modalLink = document.createElement("a");
     modalLink.textContent = "Go back to the home page";
     modalLink.href = "https://www.youtube.com";
-    const runningIcon = document.createElement("img");
-    runningIcon.src = "./assets/runningIcon.svg";
-    runningIcon.alt = "";
 
+    const runningIcon = document.createElement("img");
+    runningIcon.src = chrome.runtime.getURL("./assets/runningIcon.png");
+    runningIcon.alt = "";
+    runningIcon.width = 20;
+    runningIcon.height = 20;
     modalLink.appendChild(runningIcon);
+
+    modalWrapper.append(warningIcon, modalHeader, modalParagraph, modalLink);
+
+    if (body) {
+      body.append(modalWrapper, backdrop);
+    }
   };
 
   chrome.runtime.onMessage.addListener(
     async (request, sender, sendResponse) => {
       if (request.action === "shorts off") {
+        console.log("jjjjj");
+        showModal(
+          "You're not allowed to watch that. Hey there, future piano virtuoso! Drop that video like a hot potato! We've got a grand plan, and it involves tickling those ivories, not watching videos about leverage. Leverage? Pfft, who needs it when you've got fingers ready to dance across piano keys like they're at a ballroom party? So, close that tab, shoo away any distractions, and let's get jamming! The only leverage you need right now is the kind that helps you lift your musical spirit higher. Now go on, show that piano who's boss! No more video distractions, just you and the keys! 🎹😄"
+        );
         removeShorts();
         sendResponse({ result: "deleted" });
       } else if (request.type === "new video") {
         const response = await fetch(
           `http://127.0.0.1:5000/youtube_transcript?videoId=${request.videoId}`
         );
-        if (response.ok) {
-          const data = await response.json();
-          const inputData = await chrome.storage.local.get("textInput");
-          // perform client side validation to ensure that the inputs follow the format that the ai accepts
-          const requestBody = { Goal: inputData, videoTranscript: data };
-          const geminiResponse = await fetch("http://127.0.0.1:5000/gemini", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(requestBody),
-          });
-          if (response.ok) {
-            const geminiOutput = await geminiResponse.json();
-            if (geminiOutput.verdict === "Not Allowed") {
-              showModal(geminiOutput.encouragement);
-            }
-          }
-        }
+        showModal(
+          "You're not allowed to watch that. Hey there, future piano virtuoso! Drop that video like a hot potato! We've got a grand plan, and it involves tickling those ivories, not watching videos about leverage. Leverage? Pfft, who needs it when you've got fingers ready to dance across piano keys like they're at a ballroom party? So, close that tab, shoo away any distractions, and let's get jamming! The only leverage you need right now is the kind that helps you lift your musical spirit higher. Now go on, show that piano who's boss! No more video distractions, just you and the keys! 🎹😄"
+        );
+        // if (response.ok) {
+        //   const data = await response.json();
+        //   const inputData = await chrome.storage.local.get("textInput");
+        //   // perform client side validation to ensure that the inputs follow the format that the ai accepts
+        //   const requestBody = {
+        //     goal: inputData.textInput,
+        //     videoTranscript: data.transcript,
+        //   };
+        //   const geminiResponse = await fetch("http://127.0.0.1:5000/gemini", {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(requestBody),
+        //   });
+        //   if (response.ok) {
+        //     const geminiOutput = await geminiResponse.json();
+        //     if (geminiOutput.verdict === "Not Allowed") {
+        //       showModal(geminiOutput.encouragement);
+        //     }
+        //   }
+        // }
       }
     }
   );
